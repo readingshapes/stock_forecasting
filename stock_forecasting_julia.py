8f84d22cb9cf812f6373249c6538818b76ba9507
Julia Williams <juliawilliams@Julias-MacBook-Air.local>
2023-12-04 17:04:06 -0800

    uploading data to bigquery

--- stock_forecasting_julia.py
+++ stock_forecasting_julia.py
@@ -20,6 +20,9 @@ import random
 import statistics
 import pydoc
 import mysql.connector
+import os
+#from pandas import pandas_gbq
+from pandas.io import gbq
 
 # localize time - ambiguous error
 #tz = pd.Timestamp('2023-11-05')
@@ -33,25 +36,50 @@ apple_df = ss.retype(apple_data)
 # introduce stochrsi, macd, mfi analyses
 apple_data[['stochrsi', 'macd', 'mfi']] = apple_df[['stochrsi', 'macd', 'mfi']]
 print(apple_data)
+print(apple_ticker.get_capital_gains)
 
 # temporary: working with info
 #print(apple_ticker.balance_sheet)
 #print(apple_data)
 
-
 '''
 2: load data into sql -> bigquery
 '''
-'''
-# establish sql connection
-sql_db = mysql.connector.connect (
-   # host
-   # user
-   # password
 
+SCOPES = [
+    'https://www.googleapis.com/auth/cloud-platform',
+    'https://www.googleapis.com/auth/drive',
+]
+
+from google.oauth2 import service_account
+from google.cloud import bigquery
+credentials = service_account.Credentials.from_service_account_file(
+    'black-vehicle-406619-bf2e31773163.json')
+
+
+df = pd.read_gbq(
+    "SELECT my_col FROM `stock_ds.20yrs_stockdata`",
+    project_id='black-vehicle-406619',
+    credentials=credentials,
 )
-'''
 
+project_id = 'black-vehicle-406619'
+dataset = 'stock_ds'
+table_id = '20yrs_stockdata'
+client = bigquery.Client(credentials=credentials, project=project_id)
+
+if df.empty:
+   job = client.load_table_from_dataframe(apple_data, table_id)
+   job.result()
+   print("There are {0} rows added/changed".format(len(apple_data)))
+else:
+   changes = apple_data[~apple_data.apply(tuple, 1).isin(df.apply(tuple, 1))]
+   job = client.load_table_from_dataframe(changes, table_id)
+   job.result()
+   print("There are {0} rows added/changed".format(len(changes)))
+
+
+'''
 # import bigquery + service account
 from google.cloud import bigquery
 from google.oauth2 import service_account
@@ -59,17 +87,14 @@ from google.oauth2 import service_account
 # connect to bigquery
 #service_acc = "cloud-workflow-sa-25545f81@nifty-might-404319.iam.gserviceaccount.com"
 creds = service_account.Credentials.from_service_account_file('nifty-might-404319-b6aac4c63637.json')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'nifty-might-404319-b6aac4c63637.json'
 project_id = 'nifty-might-404319'
 client = bigquery.Client(credentials=creds, project=project_id)
+table_id = 'nifty-might-404319.
+'''
 
-# connection id
-# FROM nifty-might-404319:us-west2:instance-9999
 
-sql = """
-SELECT *
-    FROM nifty-might-404319
-    WHERE = '{0}'
-    """.format(data_date)
+
 
 
 
