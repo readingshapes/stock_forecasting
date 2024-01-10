121af7500b174a60c8831bb70be7b137d9227dc3
juliawill <wjulia800@gmail.com>
2024-01-10 21:32:49 +0000

    created dataframe for test set

--- stock_forecasting_julia.py
+++ stock_forecasting_julia.py
@@ -1,16 +1,10 @@
 # Julia's source Python file :)
 # !/usr/bin/python3
 
-#1: load data into source file
-
-# import yahoo finance data
 from google.cloud import bigquery
 from google.oauth2 import service_account
 import yfinance as yf
-# import stockstats data
 from stockstats import StockDataFrame as ss
-
-# import necessary libraries
 import matplotlib as mp
 import numpy as np
 import pandas as pd
@@ -29,7 +23,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Bidirectional, Dense, Dropout
 from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
 from sklearn.preprocessing import MinMaxScaler
-#from tqdm.notebook import tnrange
 from datetime import datetime
 from datetime import timedelta
 from datetime import date
@@ -37,14 +30,17 @@ import matplotlib.pyplot as graph
 import time
 
 def main():
+    # --- DOWNLOAD YAHOO FINANCE DATA ---
+
     # download data for apple stock
     apple_ticker = yf.Ticker("AAPL")
     # download data from past 20 years
-    apple_data = yf.download("AAPL", start='2004-01-01', interval='1d')
+    #apple_data = yf.download("AAPL", start='2004-01-01', interval='1d')
+    apple_data = yf.download("GOOGL" , start = "2018-01-01" , interval = '1d')
     apple_df = ss.retype(apple_data)
     #print(apple_data)
 
-    #2: load data into sql -> bigquery
+    # --- LOAD DATA INTO BIGQUERY ---
 
     # import google cloud service account and bigquery
     from google.oauth2 import service_account
@@ -68,33 +64,32 @@ def main():
     client_table = client.dataset(dataset_id, project=project_id).table(table_id)
     number_rows = client.get_table(client_table).num_rows
 
+    # --- CREATE LSTM MODEL ---
+
     # determine slice of data for training set (~70%)
-    train_vals_cutoff = int(len(apple_data) * 0.7)
+    train_vals_cutoff = apple_df[apple_data.index >= '2022-01-01'].shape[0]
+    #train_vals_cutoff = int(len(apple_data) * 0.7)
         
     # determine feature length and target vals for lstm model
     time_step_vals, target_vals = features_targets(apple_data["close"].values, 20)
     # call function to create model for first test length
-    scaler = MinMaxScaler(feature_range=(0,1))
+    scaler = MinMaxScaler(feature_range=(0, 200))
     apple_df['close_price_scaled'] = scaler.fit_transform(apple_df['close'].values.reshape(-1, 1))
-    model, loss, pre, predict, Y_test = create_model(time_step_vals, target_vals, apple_df["close"].values, train_vals_cutoff, scaler)
+    #model, loss, pre, predict, Y_test, scaler = create_model(time_step_vals, target_vals, apple_df["close"].values, train_vals_cutoff, scaler)
+    create_model(time_step_vals, target_vals, apple_df, apple_df["close"].values, train_vals_cutoff, scaler)
     # call function to predict lstm model
     #predict = lstm_predict(model, apple_df["close"].values, forecast_date='2024-01-03')
-
     
-    #graph.plot(apple_df[train_vals_cutoff + 20:], scaler.inverse_transform(Y_test.reshape(-1, 1)), label='Actual Prices')
-    #graph.plot(apple_df[train_vals_cutoff + 20:], predict, label='Predicted Prices')
-    #print(apple_df[(train_vals_cutoff + 20):])
-    #graph.plot(apple_df[(train_vals_cutoff + 20):].index, predict, label="Predicted")
-    graph.plot(apple_df[(train_vals_cutoff + 20):].index, predict[:, 0], label="Predicted")
-
+    '''
     graph.xlabel('Date')
     graph.ylabel('Stock Price')
     graph.legend()
     graph.ion()
     graph.show(block=True)
-    graph.savefig('predicted_stock_prices.png')
+    graph.savefig('predicted_stock_prices_lstm.png')
+    '''
 
-#3: -- FUNCTIONS NEEDED FOR LSTM MODEL CREATION --
+# --- FUNCTIONS NEEDED FOR LSTM MODEL CREATION ---
 
 # create feature length list and target list - needed for lstm model
 def features_targets(data, feature_length):
@@ -118,12 +113,13 @@ def features_targets(data, feature_length):
     return time_step_list, close_label_list
 
 # -- CREATE BIDIRECTIONAL LSTM --
-def create_model(X, Y, data, train_test_slice, scaler):
+
+def create_model(X, Y, df, data, train_test_slice, scaler):
     # training set: set to train the machine learning model
     # testing set: set used to test model after model has been trained
     # train set is 70% of data, test set is the rest (30%)
-    X_train, X_test = X[:train_test_slice], X[train_test_slice:]
-    Y_train, Y_test = Y[:train_test_slice], Y[train_test_slice:]
+    X_train, X_test = X[:-train_test_slice], X[-train_test_slice:]
+    Y_train, Y_test = Y[:-train_test_slice], Y[-train_test_slice:]
 
     # initialize empty model where nodes have input and output
     model = Sequential()
@@ -132,25 +128,30 @@ def create_model(X, Y, data, train_test_slice, scaler):
     model.add(LSTM(50, recurrent_dropout=0.1))
     # add dropout and dense layers
     model.add(Dropout(0.2))
-    model.add(Dense(2, activation='elu'))
+    model.add(Dense(1, activation='linear'))
+
     # optimize model using stochastic gradient descent to train model
-    optimize = tf.keras.optimizers.SGD(learning_rate = 0.001)
+    optimize = tf.keras.optimizers.SGD(learning_rate = 0.002)
     # compile model
-    #model.compile(loss='mean_squared_error', optimizer=optimize, metrics=[tf.keras.metrics.RootMeanSquaredError(name='rmse')])
-    model.compile(loss='mean_squared_error', optimizer=optimize)
+    model.compile(loss='mean_squared_error', optimizer=optimize, metrics=[tf.keras.metrics.RootMeanSquaredError(name='rmse')]) 
     # save model weights when needed
-    #weights = ModelCheckpoint("best_weights.h5", monitor='val_loss', save_best_only=True, save_weights_only=True)
+    weights = ModelCheckpoint("best_weights.h5", monitor='val_loss', save_best_only=True, save_weights_only=True)
     # adjust learning rate when needed
-    #reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.25,patience=4, min_lr=0.00001,verbose = 1)
-    # !!!changed epochs from 10 to 1
-    # fit model
-    #history = model.fit(X_train, Y_train, epochs=1, batch_size = 1, verbose=1, shuffle=False, validation_data=(X_test , Y_test), callbacks=[reduce_lr, weights])
-    loss = model.evaluate(X_test, Y_test)
-    print(f'Mean Squared Error on Test Set: {loss}')
-    pred = model.predict(X_test)
-    preds = scaler.inverse_transform(pred)
-   
-    return model, loss, pred, preds, Y_test
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.25, patience=4, min_lr=0.00001, verbose = 1)
+
+    # one epoch completes when the entire training dataset is processed once by the model
+    model.fit(X_train, Y_train, epochs=1, batch_size = 1, verbose=1, shuffle=False, validation_data=(X_test, Y_test), callbacks=[reduce_lr, weights])
+    Actual = scaler.inverse_transform(Y_test)
+    Predictions = model.predict(X_test)
+    Predictions = scaler.inverse_transform(Predictions)
+    Actual = np.squeeze(Actual , axis = 1)
+
+    # create test df
+    index_dates = data[-train_test_slice:]
+    index_dates = pd.to_datetime(index_dates)
+    test_df = pd.DataFrame({'Date': df.index[-train_test_slice:], 'Actual': Actual, 'Predicted': Predictions.flatten()})
+    print(test_df)
+    #return model, loss, predict, predict_inv, Y_test, scaler
     
     
 main()
@@ -158,7 +159,7 @@ main()
 
 
 
-
+                                       
 
 
 
